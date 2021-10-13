7ce73549920e701413ba9d2fa15fc05b2c354c8b
unknown <miemail@dominio.com>
2021-10-12 21:56:29 -0500

    ajax_5

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
-function entrar(){
+
 
     var user=   $.trim($('#user').val());
     var pass=   $.trim($('#pass').val());
 
     //alert(user);
-
+    function entrar(){
     $.ajax({
         url: "php/consultas.php",
         type: "POST",
         dataType: "json",
-        data: {usuario: user, contraseña: pass},
+        data: {opc: '1' ,usuario: user, contraseña: pass},
         success: function(data){
             alert('esta bien');
         },
         error: function(r){
             alert('algo esta mal');
         }
-    })
+    });
     // .done(()=>{
     //     alert('todo esta bien');
     // })
@@ -25,4 +25,25 @@ function entrar(){
     // .fail(()=>{
     //     alert('algo anda mal');
     // })
+}
+
+//////////////////////////Registro////////////////////////////7
+
+function registro(){
+    var direccion = $.trim($('#direccion').val);
+    var cel       = $.trim($('#cel').val);
+    var f_n       = $.trim($('#f_n').val);
+
+    $.ajax({
+        url: 'php/consultas.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {opc: '2', usuario: user, contraseña: pass, dereccion: direccion, cel: cel, f_n: f_n},
+        success: function(data){
+            alert('exito');
+        },
+        error: function(r){
+            alert('error: '+ r);
+        }
+    });
 }
--- php/consultas.php
+++ php/consultas.php
@@ -1,53 +1,70 @@
 <?php
 
-    $nombre     = $_POST['usuario'];
-    $contraseña = $_POST['contraseña'];
+$nombre     = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+$opc        = $_POST['opc'];
 
-     $server = 'localhost';
-     $user   = 'root';
-     $pass   = '';
-     $db     = 'consultas';
+ $server = 'localhost';
+ $user   = 'root';
+ $pass   = '';
+ $db     = 'consultas';
 
-    // $conexion = (mysqli_connect($server,$user,$pass));
-    // mysqli_select_db($conexion, $db) or die ('no se encuentra la db');
-    // print ('todo esta bien');
-    
+// $conexion = (mysqli_connect($server,$user,$pass));
+// mysqli_select_db($conexion, $db) or die ('no se encuentra la db');
+// print ('todo esta bien');
+$conexion = new mysqli($server,$user,$pass,$db); //conexion
+//consulta login
+switch($opc){
+    case 1:
+        
+        $sql      = "SELECT * FROM usuario WHERE nombre = '$nombre'"; //codigo sql
 
-    $conexion = new mysqli($server,$user,$pass,$db); //conexion
-    $sql      = "SELECT * FROM usuario WHERE nombre = '$nombre'"; //codigo sql
+        $consulta = mysqli_query($conexion, $sql) or die (mysqli_error()); //hacemos la peticion
+        // echo true;
 
-    $consulta = mysqli_query($conexion, $sql) or die (mysqli_error()); //hacemos la peticion
-    // echo true;
-
-    if($fila = mysqli_fetch_array($consulta)){
-        if($contraseña == $fila['documento']){
-            echo true;
-        }else{
-            echo false;
+        if($fila = mysqli_fetch_array($consulta)){
+            if($contraseña == $fila['documento']){
+                echo true;
+            }else{
+                echo false;
+            }
         }
-    }
+        break;
 
-    // if(mysqli_fetch_array($consulta) > 0){
-    //     echo true;
-    // }else{
-    //     print false;
-    // }
+    case 2:
+        $direccion  = $_POST['dereccion'];
+        $cel        = $_POST['cel'];
+        $f_n        = $_POST['f_n'];
+        
+        $sql = "INSERT INTO usuario (nombre, documento, tel, direccion, fecha_nac)VALUE ('$nombre', '$contraseña', '$cel','$direccion', '$f_n')";
+        $consulta = mysqli_query($conexion, $sql) or die (mysqli_error());
+        echo true;
 
-    // if($conexion -> connect_error){
-    //     die ('la conexion fallo');
-    // }else{
-    //     echo 'conexion exitosa';
-    // }
-
+}
 
 
 
-    // $user = $_POST['usuario'];
+// if(mysqli_fetch_array($consulta) > 0){
+//     echo true;
+// }else{
+//     print false;
+// }
 
-    // if($user == 'ana'){
-    //     echo true;
-    // }else{
-    //     echo false;
-    // }
+// if($conexion -> connect_error){
+//     die ('la conexion fallo');
+// }else{
+//     echo 'conexion exitosa';
+// }
+
+
+
+
+// $user = $_POST['usuario'];
+
+// if($user == 'ana'){
+//     echo true;
+// }else{
+//     echo false;
+// }
 ?>
 
